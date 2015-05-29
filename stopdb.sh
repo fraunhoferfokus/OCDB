09275cb66883b4e8347526a158f3c722d59b6bf8
Martin Lasak <martin.lasak@fokus.fraunhofer.de>
2015-05-29 12:14:12 +0200

    Makes db shutdown script cross platform.

--- stopdb.sh
+++ stopdb.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname $0)
 echo "shutting down..."
-$BASEDIR/db/mongodb/bin/mongod --dbpath $BASEDIR/db --shutdown
+$BASEDIR/db/mongodb/bin/mongo --eval "db.getSiblingDB('min').shutdownServer()"
